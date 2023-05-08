c33bd531c452890fd0c497010f22db3fee7f502c
Nikita Sychev <nikitasychev@inbox.ru>
2023-05-08 14:08:51 +0500

    воалптовлап

--- Program.cs
+++ Program.cs
@@ -12,14 +12,7 @@ using System.Threading;
 
 namespace DbConnectionExample
 {
-    public class userData // создали класс для записи данных пользователя
-    {
-        public userData(string name, string phoneNumber, string mail) { _name = name;  _numberPhone = phoneNumber; _mail = mail; }
-        //создали конструктор класса
-        public string _name { get; } // свойства геттер
-        public string _mail { get; }// свойства геттер
-        public string _numberPhone { get; }// свойства геттер
-    }
+   
     delegate void myDel(string _text); //delegate 
     public class myConnectToMSSQLDB  //класс для связи с БД
     {
@@ -109,6 +102,8 @@ internal class Program
             Console.WriteLine();
         }
     }
+
+    
     static void Main(string[] args)
     {
         myDel putText; //делегат
@@ -121,7 +116,6 @@ internal class Program
         string name;
         string phone;
         string mail;
-        List<userData> users = new List<userData>(); // создали лист users для записи данных
         Regex regPhone = new Regex(@"^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$"); // создаем регексы
         Regex regName = new Regex(@"^[a-zA-Z]+$");
         Regex regMail = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
@@ -140,10 +134,7 @@ internal class Program
             data = Console.ReadLine();
             MatchCollection matchFindMail = regMail.Matches(data);
             mail = matchFindPhone[0].ToString();
-            userData user = new userData(name, mail, phone); //создаем объект класса
-            users.Add(user);
             string user_list = $"'{name}','{phone}','{mail}'"; // добавляем данные в лист
-            putText.Invoke(user_list);// метод вызова делегата 
           
         } while (true);
 
